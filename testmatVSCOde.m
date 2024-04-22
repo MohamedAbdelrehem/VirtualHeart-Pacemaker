814ce9735679a6c4f95fcd82481eeeb58bf137f3
Mohamed Abdelrehem <mohamedabdelrehem32@gmail.com>
2024-04-22 23:24:33 +0200

    Refactor CSV export logic and fix formatting issue in testmatVSCOde.m

--- testmatVSCOde.m
+++ testmatVSCOde.m
@@ -56,6 +56,11 @@ for i = 1:length(files)
                             % Check if cell_value contains a comma
                             if ischar(cell_value) && contains(cell_value, ',')
                                 fprintf(fid, '"%s",', cell_value); % Write the string enclosed in double quotes
+                            % check if there is array in the cell and save it as a string with comma
+                            elseif isnumeric(cell_value)
+                                if length(cell_value) > 1
+                                    fprintf(fid, '%s,', mat2str(cell_value));
+                                end
                             else
                                 % Format based on data type
                                 if isnumeric(cell_value)
@@ -80,4 +85,5 @@ for i = 1:length(files)
             end
     end
 end
+
 
