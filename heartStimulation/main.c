3365bb0256a87f83b0fe2232be9e98c0b5f6c6ef
=ahhassan88 <=>
2024-04-30 00:50:13 +0300

    heart stimulation

---
+++ heartStimulation/main.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "heart_model.h"
+
+typedef struct test{
+double x;
+double y;
+
+}testing;
+int main()
+{
+//  path p1;
+//  node_def n1;
+//testing sd;
+
+    printf("Hello world!\n");
+// heart_model(&n1,&p1);
+    return 0;
+}
