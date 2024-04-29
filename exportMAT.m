286055c4a6c499440ca90769615e6203daa23f09
Mohamed Abdelrehem <mohamedabdelrehem32@gmail.com>
2024-04-29 20:59:52 +0300

    Refactor CSV export logic and fix formatting issues in ConvertMATtoCSVstrings.m

--- exportMAT.m
+++
@@ -1,8 +0,0 @@
-%writematrix(egm_table,'egm_table.xlsx','Sheet',1)
-
-
- data=load('FileName');
- f=fieldnames(data);
- for k=1:size(f,1)
-   writematrix('FileName.xlsx',data.(f{k}),f{k})
- end
---
+++ readcsv.c
@@ -0,0 +1,20 @@
+// D:/github/VirtualHeart&Pacemaker/cases/EP_AVNRT/egm_table.csv
+// C program for the above approach
+#include <conio.h>
+#include <stdio.h>
+#include <string.h>
+
+// Driver Code
+int main()
+{
+    // Substitute the full file path
+    // for the string file_path
+    FILE *fp = fopen("D:/github/VirtualHeart&Pacemaker/cases/EP_AVNRT/egm_table.csv", "r");
+
+    if (!fp)
+        printf("Can't open file\n");
+
+    else
+    {
+        // all of data is save between "" use this to get data
+    }
