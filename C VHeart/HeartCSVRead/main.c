be3eac300f730bf5acb5137f58da91423876a766
Mohamed Abdelrehem <mohamedabdelrehem32@gmail.com>
2024-05-03 20:14:13 +0300

    Refactor main.c to improve code organization and add support for UI

--- C VHeart/HeartCSVRead/main.c
+++ C VHeart/HeartCSVRead/main.c
@@ -1,23 +1,28 @@
+#define UION 1
 
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#include "./nodes/dataStructure.h"
+#include "./dataStructure.h"
 #include "./matlabRead/matlabRead.h"
 #include "./nodes/nodePath.h"
 #include "./nodes/nodeTable.h"
 #include "./nodes/nodePosition.h"
+#include "./nodes/nodePathLoaction.h"
 
 #include "constants.h"
+#if UION
 // include the SDL2 library
 #include "MYsdlWindow.h"
-
+#endif
 //^ @MohamedAbdelrehem to @ahhassan88 : I have added the following variables as
 // extern to be able to use them in the main file globally
 extern node_def nodeTable[100];
 extern node_path nodePathtable[100];
+extern node_location nodeLocationTable[100];
+extern node_path_location nodePathLocationTable[100];
 
 // number of nodes and pathes that will be read from the csv files
 int numberofnodes = 0;
@@ -60,13 +65,19 @@ int main(int argc, char *argv[])
   //? print nodeTable to check
   //? printNodeLocationTable(&numberofnodesLocation);
 
+  //~node path table cordination initialization
+  saveToNodePathLocationStruct(&numberofnodesPathes);
+#if UION
   //**--------------------------------------------------------------------------------**//
   //~ UI
   UI();
+#endif
   //**--------------------------------------------------------------------------------**//
 
+#if !UION
   //* print the node table and path table to check without the SDL
-  // printData();
+  printData();
+#endif
   return 0;
 }
 
@@ -75,11 +86,12 @@ void printData()
   system("cls");
   while (1)
   {
-
-    printNodeTable(&numberofnodes);
-    printNodePathTable(&numberofnodesPathes);
-    printNodeLocationTable(&numberofnodes);
-
+    // TODO: take input from the user to print the data with switch case and go back to the main menu when the user press q
+    //? printNodeTable(&numberofnodes);
+    //? printNodePathTable(&numberofnodesPathes);
+    //? printNodeLocationTable(&numberofnodes);
+    //? printNodePathLocationTable(&numberofnodesPathes);
+    //? to print the data in the same place by moving the cursor to the top left
     printf("\033[0;0H");
   }
 }
