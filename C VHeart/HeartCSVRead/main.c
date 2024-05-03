ed3b31688c6de51b2268504fd6467e860f693cfb
Mohamed Abdelrehem <mohamedabdelrehem32@gmail.com>
2024-05-04 02:09:01 +0300

    Refactor main.c to improve code organization and add support for UI

--- C VHeart/HeartCSVRead/main.c
+++ C VHeart/HeartCSVRead/main.c
@@ -1,9 +1,11 @@
-#define UION 1
+#include "../Settings.h"
 
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+// used to get if the key hit while printing the data
+#include <conio.h> /* getch() and kbhit() */
 
 #include "./dataStructure.h"
 #include "./matlabRead/matlabRead.h"
@@ -31,8 +33,8 @@ int numberofnodesLocation = 0;
 
 // to check if the window is initialized or not
 
-void printData();
-
+void printData(void);
+void menu(void);
 // will run in main file to initialize the window with int argc, char *argv[]
 int main(int argc, char *argv[])
 {
@@ -70,7 +72,7 @@ int main(int argc, char *argv[])
 #if UION
   //**--------------------------------------------------------------------------------**//
   //~ UI
-  UI();
+  mainUI();
 #endif
   //**--------------------------------------------------------------------------------**//
 
@@ -84,14 +86,77 @@ int main(int argc, char *argv[])
 void printData()
 {
   system("cls");
+  char choice;
+  menu();
+  printf("Enter your choice: ");
+
+  scanf(" %c", &choice);
+
+  // TODO: take input from the user to print the data with switch case and go back to the main menu when the user press q
+  system("cls");
   while (1)
   {
-    // TODO: take input from the user to print the data with switch case and go back to the main menu when the user press q
-    //? printNodeTable(&numberofnodes);
-    //? printNodePathTable(&numberofnodesPathes);
-    //? printNodeLocationTable(&numberofnodes);
-    //? printNodePathLocationTable(&numberofnodesPathes);
+    menu();
+
+    switch (choice)
+    {
+
+    case '1':
+      printNodeTable(&numberofnodes);
+      break;
+    case '2':
+      printNodePathTable(&numberofnodesPathes);
+      break;
+    case '3':
+      printNodeLocationTable(&numberofnodes);
+      break;
+    case '4':
+      printNodePathLocationTable(&numberofnodesPathes);
+      break;
+    case 'q':
+    case 'Q':
+      return; // Exit the function
+    default:
+      printf("Invalid choice. Please try again.\n");
+    }
+    if (kbhit())
+    {
+      choice = getch();
+      system("cls");
+    }
     //? to print the data in the same place by moving the cursor to the top left
     printf("\033[0;0H");
   }
+}
+
+void menu(void)
+{
+  printf("          |  \\ \\ | |/ /\n");
+  printf("          |  |\\ `' ' /\n");
+  printf("          |  ;'aorta \\      / , pulmonary\n");
+  printf(" superior | |   (  `-.;_,-' '-' ,\n");
+  printf("vena cava | `,   `-._       _,-'_\n");
+  printf("          |,-`.    `.)    ,<_,-'_, pulmonary\n");
+  printf("         ,'    `.   /   ,'  `;-' _,  veins\n");
+  printf("        ;        `./   /`,    \\-'\n");
+  printf("        | right   /   |  ;\\   |\n");
+  printf("        | atrium ;_,._|_,  `, ' \\\n");
+  printf("        |        \\    \\ `       `, \n");
+  printf("        `      __ `    \\   left  ;,\n");
+  printf("         \\   ,'  `      \\,  ventricle\n");
+  printf("          \\_(            ;,      ;;\n");
+  printf("          |  \\           `;,     ;;\n");
+  printf(" inferior |  |`.          `;;,   ;'\n");
+  printf("vena cava |  |  `-.        ;;;;,;'\n");
+  printf("          |  |    |`-.._  ,;;;;;'\n");
+  printf("          |  |    |   | ``';;;'  FL\n");
+  printf("                  aorta\n\n");
+
+  printf("** Make sure Terminal is full screen and font size is small** \n\n");
+
+  printf("1. Print Node Table\n");
+  printf("2. Print Node Path Table\n");
+  printf("3. Print Node Location Table\n");
+  printf("4. Print Node Path Location Table\n");
+  printf("q. Quit\n");
 }
